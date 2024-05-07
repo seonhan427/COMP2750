514f2ccd944498563ea565bdf93eece677446065
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2024-05-07 12:29:44 +1000

    Add files via upload

---
+++ comp2750-a3-group/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import {Picker} from '@react-native-picker/picker'; 
+
+export default function App() {
+
+
+  return (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.row1}>
+    <Text style={styles.heading}>POGS</Text>
+    </View>
+
+    <View style={{justifyContent: 'center', alignContent: 'center'}}>
+    <Image 
+      style={styles} 
+      source={require('./assets/illustration-organic-farming-concept-with-farmer_23-2148424155.jpg')}
+    />
+    </View>
+
+  </SafeAreaView>
+  );}
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ffffcc'
+    },
+      heading: {
+        fontSize:30,
+        textAlign:'center',
+        marginTop: 30
+      },
+  });
